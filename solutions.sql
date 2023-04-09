bfbad32342c27cdb996ef9d2f288a08cd7bf712e
Vyacheslav Sokolov <vyacheslavsokolov.wk@gmail.com>
2023-04-09 15:43:29 +0300

    Feat: add minor changes

--- solutions.sql
+++ solutions.sql
@@ -1,9 +1,15 @@
 -- Задание 1. Вывести имена всех людей, которые есть в базе данных авиакомпаний.
 
 SELECT name
-from passenger
+from passenger;
 
 -- Задание 2. Вывести названия всеx авиакомпаний.
 
 SELECT name
-from company
+from company;
+
+-- Задание 3. Вывести все рейсы, совершенные из Москвы.
+
+SELECT *
+FROM trip
+WHERE town_from = "Moscow";
